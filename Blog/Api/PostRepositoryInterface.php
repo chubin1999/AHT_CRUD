5ed2c7e54c96efa6c294fb46218f98d749bc6110
chubin1999 <phachgia1999@gmail.com>
2021-01-08 16:30:43 +0700

    upload crud

---
+++ Blog/Api/PostRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AHT\Blog\Api;
+
+interface PostRepositoryInterface {
+	public function save(\AHT\Blog\Api\Data\PostInterface $Post);
+
+	public function getById($PostId);
+
+	public function delete(\AHT\Blog\Api\Data\PostInterface $Post);
+
+	public function deleteById($PostId);
+}
---
+++ Blog/Setup/InstallData.php
@@ -0,0 +1,26 @@
+<?php
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $data = [
+            [
+                'name'         => 'How to Create SQL Setup Script in Magento 2',
+                'url_key'       => '/hi',
+                'image'   => '',
+                'created_at'    => '',
+                'status'        => 1
+            ],
+         
+        ];
+
+        foreach ($data as $value) {
+            $setup->getConnection()->insertForce($setup->getTable('aht_blog_post'), $value);
+        }
+    }
+}
